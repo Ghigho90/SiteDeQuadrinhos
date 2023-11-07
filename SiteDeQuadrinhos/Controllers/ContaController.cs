2f39926fab1f955292e61d227817969bb8a07042
Ghigho90 <ghigho33@gmail.com>
2023-11-07 05:40:53 -0300

    login

---
+++ SiteDeQuadrinhos/Controllers/ContaController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SiteDeQuadrinhos.Data;
+using SiteDeQuadrinhos.Models;
+
+namespace SiteDeQuadrinhos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContaController : ControllerBase
+    {
+        private readonly UserManager<UsuarioModel> _userManager;
+        private readonly SignInManager<UsuarioModel> _signInManager;
+        private readonly SiteDeQuadrinhosDBContex _context;
+
+        public ContaController(UserManager<UsuarioModel> userManager, SignInManager<UsuarioModel> signInManager, SiteDeQuadrinhosDBContex context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel login)
+        {
+            var user = await _userManager.FindByEmailAsync(login.Email);
+
+            if(user  != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, login.Senha);
+                if (passwordCheck)
+                {
+                    var result = _signInManager.PasswordSignInAsync(user, login.Senha, false, false);
+                    if (result.IsCompletedSuccessfully)
+                    {
+                        return Ok(result);
+                    }
+                }
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ SiteDeQuadrinhos/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SiteDeQuadrinhos.Models
+{
+    public class LoginModel
+    {
+        [Display(Name = "Endereço de e-mail")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+    }
+}
